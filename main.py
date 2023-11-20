601269b770990d698f6da79ac02863bf337ae506
Christian Pérez <christiandp@live.com>
2023-11-20 10:52:50 -0600

    código MIPS en GUI

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ from util.generate_tree import generate_parse_tree
 from compiler_files.custom_visitor import CustomVisitor
 from antlr4.error.ErrorListener import ErrorListener
 
+from ThreeAdress_parser.ThreeAddressCodeLexer import  ThreeAddressCodeLexer
+from ThreeAdress_parser.ThreeAddressCodeParser import ThreeAddressCodeParser
+from ci_to_cm.custom_visitor import CI2MPIPSVisitor
+
 class CustomErrorListener(ErrorListener):
     def __init__(self, error_callback):
         super().__init__()
@@ -333,6 +337,43 @@ class MainWindow(QMainWindow):
                 if not (self.ci_frame in self.hsplit.children()):
                     self.hsplit.replaceWidget(0, self.ci_frame)
                 # self.hsplit.replaceWidget(0, self.ci_frame)
+
+                # Parte de codigo intermedio a codigo de maquina
+                input_stream = FileStream('compiler_files/outputs/intern_code.ac')
+
+                lexer = ThreeAddressCodeLexer(input_stream)
+                lexer.addErrorListener(CustomErrorListener(self.error_callback))
+
+                token_stream = CommonTokenStream(lexer)
+                token_stream.fill()
+                parser = ThreeAddressCodeParser(token_stream)
+                parser.addErrorListener(CustomErrorListener(self.error_callback))
+
+                tree = parser.program()
+
+                # check errors
+                if parser.getNumberOfSyntaxErrors() > 0:
+                    return self.error_callback(parser.getNumberOfSyntaxErrors())
+                
+                ci_to_cmips_visitor = CI2MPIPSVisitor()
+
+                try:
+                    result = ci_to_cmips_visitor.visit(tree)
+                    print(result)
+                    with open('ci_to_cm/outputs/mips_code.asm', 'r') as f:
+                        mips_code = f.read()
+
+                    self.mips_text.setText(mips_code)
+                    if not (self.mips_frame in self.hsplit.children()):
+                        self.hsplit.replaceWidget(0, self.mips_frame)
+                    # self.hsplit.replaceWidget(0, self.mips_frame)
+                    
+                except Exception as e:
+                    print(e)
+                    self.terminal_text.append("> Compilation to MIPS failed")
+                    if not (self.terminal_frame in self.hsplit.children()):
+                        self.hsplit.replaceWidget(0, self.terminal_frame)
+                    # self.terminal_text.setText("> Compilation to MIPS failed")
             except:
                 #
                 pass
@@ -439,6 +480,8 @@ class MainWindow(QMainWindow):
         side_bar_layout.addWidget(terminal_label)
         ci_label = self.get_side_bar_label('gui/icons/ci.png', 'CI')
         side_bar_layout.addWidget(ci_label)
+        mips_label = self.get_side_bar_label('gui/icons/MIPS.png', 'MIPS')
+        side_bar_layout.addWidget(mips_label)
 
         self.side_bar.setLayout(side_bar_layout)
 
@@ -548,6 +591,31 @@ class MainWindow(QMainWindow):
         ci_frame_layout.addWidget(self.ci_text)
         self.ci_frame.setLayout(ci_frame_layout)
 
+        # MIPS (assembly) view
+        self.mips_frame = self.get_frame()
+        self.mips_frame.setMaximumWidth(400)
+        self.mips_frame.setMinimumWidth(200)
+
+        mips_frame_layout = QVBoxLayout()
+        mips_frame_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        mips_frame_layout.setContentsMargins(0, 10, 0, 0)
+        mips_frame_layout.setSpacing(0)
+
+        self.mips_text = QTextEdit()
+        self.mips_text.setReadOnly(True)
+        self.mips_text.setFont(self.window_font)
+        self.mips_text.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.mips_text.setStyleSheet('''
+                                        background-color: #21252b;
+                                        border-radius: 5px;
+                                        border: none;
+                                        padding: 5px;
+                                        color: #D3D3D3;
+                                        ''')
+        mips_frame_layout.addWidget(self.mips_text)
+        self.mips_frame.setLayout(mips_frame_layout)
+
+
         # setup layout
         tree_frame_layout.addWidget(self.tree_view)
         self.tree_frame.setLayout(tree_frame_layout)
@@ -617,6 +685,9 @@ class MainWindow(QMainWindow):
         elif type == 'CI':
             if not (self.ci_frame in self.hsplit.children()):
                 self.hsplit.replaceWidget(0, self.ci_frame)
+        elif type == 'MIPS':
+            if not (self.mips_frame in self.hsplit.children()):
+                self.hsplit.replaceWidget(0, self.mips_frame)
         
         if self.current_side_bar == type:
             frame = self.hsplit.children()[0]
